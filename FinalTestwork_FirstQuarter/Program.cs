38841ef147a2113836c82ba6ee959a1f1fa0a4af
Korolev_Kirill <tk.kirg14@gmail.com>
2023-03-27 02:42:37 +0300

    Solved task

--- FinalTestwork_FirstQuarter/Program.cs
+++ FinalTestwork_FirstQuarter/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class Program
+{
+    public static void Main()
+    {
+        Random random = new Random();
+        int length = random.Next(2, 6);
+        string[] wordsInput = new string[length];
+        string[] threeLetterWords = new string[0];
+        Console.WriteLine("Слов в массиве " + length.ToString());
+        for (int i = 0; i < wordsInput.Length; i++)
+        {
+            Console.WriteLine("Введите слово");
+            wordsInput[i] = Console.ReadLine().ToString();
+            if (wordsInput[i].Length <=  3)
+            {
+                threeLetterWords = threeLetterWords.Append(wordsInput[i]).ToArray();
+            }
+        }
+
+        foreach (string word in threeLetterWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+
+}
